4995cd9747afd0b741929edd5c6bbb01823c9d1c
Эльчин Кулиев <el.kuliyev@gmail.com>
2025-10-30 11:49:17 +0000

    когда я нажимаю download cv ничего не происходит. размести любое pdf как

--- src/components/ats-button.tsx
+++ src/components/ats-button.tsx
@@ -4,14 +4,15 @@ import { Download } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 export function AtsButton() {
-  const handleDownload = () => {
-    window.print();
-  };
-
   return (
-    <Button onClick={handleDownload} variant="default" className="no-print">
-      <Download className="mr-2 h-4 w-4" />
-      Download CV
+    <Button asChild variant="default" className="no-print">
+      <a
+        href="https://tryhackme-certificates.s3-eu-west-1.amazonaws.com/THM-XKWGH6IECR.pdf"
+        download="sample-cv.pdf"
+      >
+        <Download className="mr-2 h-4 w-4" />
+        Download CV
+      </a>
     </Button>
   );
 }
